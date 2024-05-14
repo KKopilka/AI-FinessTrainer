5282092c87240cf90f56f77e97d5eba704f350e8
Yana <yastepankoa@gmail.com>
2024-05-14 20:19:03 +0300

    updated files

--- app/live.py
+++ app/live.py
@@ -112,8 +112,6 @@
 # #     # out_recorder_factory=out_recorder_factory
 # # )
 
-
-
 import streamlit as st
 import time
 import av
@@ -121,7 +119,7 @@ from streamlit_webrtc import VideoHTMLAttributes, webrtc_streamer
 from streamlit_option_menu import option_menu
 import sys
 st.set_page_config(page_title="AI-FitnessTrainer", page_icon=":basketball:", layout="wide")
-sys.path.append('/AI-Trainer')
+sys.path.append('D:\AI-Trainer')
 from exercise_analyzer import ExerciseAnalyzer
 from constants import _TEXT_BICEPS, _TEXT_PUSH_UP, _TEXT_REVERSE_PUSH_UP, _TEXT_SQUAT
 
@@ -133,7 +131,7 @@ def choose_exercise(exercise_key):
     st.session_state['exercise'] = exercise_key
 
 if st.session_state['exercise'] is None:
-    st.header('Интеллектуальный помощник по фитнесу :rocket:')
+    st.header('Интеллектуальный помощник по фитнесу :rocket:', divider='gray')
     st.subheader('Добро пожаловать в AI-FitnessTrainer!')
     st.write('''
     В этом приложении вы можете выбрать различные упражнения для анализа вашей техники и получения обратной связи в реальном времени.
@@ -147,55 +145,54 @@ if st.session_state['exercise'] is None:
         'Обратные отжимания': 'reverse_push_up'
     }
 
-    for key, value in exercise_options.items():
-        st.button(key, on_click=choose_exercise, args=(value,))
+    cols = st.columns(len(exercise_options))
+    for col, (key, value) in zip(cols, exercise_options.items()):
+        col.button(key, on_click=choose_exercise, args=(value,))
 
 else:
-    left_column, right_column = st.columns(2)
-
     if st.session_state['exercise'] == 'front_squat':
+        st.header('Фронтальные приседания :rocket:', divider='gray')
+        left_column, right_column = st.columns(2, gap='medium')
         with left_column:
-            st.header('Фронтальные приседания')
-            st.image("./assets/frontalniye-prisedaniya.jpeg", width=400)
+            st.image("./assets/frontalniye-prisedaniya.jpeg", use_column_width=True)
         with right_column:
             st.write(_TEXT_SQUAT)
-            # st.write_stream(stream_data(_TEXT_SQUAT))
             analyzer = ExerciseAnalyzer('front_squat')
 
     elif st.session_state['exercise'] == 'push_up':
+        st.header('Отжимания с широкой постановкой рук :rocket:', divider='gray')
+        left_column, right_column = st.columns(2, gap='medium')
         with left_column:
-            st.header('Отжимания с широкой постановкой рук')
-            st.image("./assets/push_up.jpg", width=400)
+            st.image("./assets/push_up.jpg", use_column_width=True)
         with right_column:
-            # st.write_stream(stream_data(_TEXT_PUSH_UP))
             st.write(_TEXT_PUSH_UP)
             analyzer = ExerciseAnalyzer('push_up')
 
     elif st.session_state['exercise'] == 'biceps':
+        st.header('Упражнение с гантелями на бицепс :rocket:', divider='gray')
+        left_column, right_column = st.columns(2, gap='medium')
         with left_column:
-            st.header('Упражнение с гантелями на бицепс')
-            st.image("./assets/biceps.jpg", width=400)
+            st.image("./assets/biceps.jpg", use_column_width=True)
         with right_column:
-            # st.write_stream(stream_data(_TEXT_BICEPS))
             st.write(_TEXT_BICEPS)
             analyzer = ExerciseAnalyzer('biceps')
 
     elif st.session_state['exercise'] == 'reverse_push_up':
+        st.header('Обратные отжимания :rocket:', divider='gray')
+        left_column, right_column = st.columns(2, gap='medium')
         with left_column:
-            st.header('Обратные отжимания')
-            st.image("./assets/reverse_push_up.jpg", width=450)
+            st.image("./assets/reverse_push_up.jpg", use_column_width=True)
         with right_column:
-            # st.write_stream(_TEXT_REVERSE_PUSH_UP)
             st.write(_TEXT_REVERSE_PUSH_UP)
             analyzer = ExerciseAnalyzer('reverse_push_up')
 
 
     def video_frame_callback(frame: av.VideoFrame):
         frame = frame.to_ndarray(format="rgb24") 
-        # try:
-        frame, correctCount, incorrectCount = analyzer.analyze_video(frame) 
-        # except BaseException as b:
-            # print(b)
+        try:
+            frame, correctCount, incorrectCount = analyzer.analyze_video(frame) 
+        except BaseException as b:
+            print(b)
         return av.VideoFrame.from_ndarray(frame, format="rgb24")
 
     ctx = webrtc_streamer(
