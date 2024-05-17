0f620e9497fc589ebb9e501e51b12edd199d6258
Yana <yastepankoa@gmail.com>
2024-05-17 16:23:29 +0300

    fix

--- app/live.py
+++ app/live.py
@@ -5,7 +5,7 @@ from streamlit_webrtc import VideoHTMLAttributes, webrtc_streamer, WebRtcMode
 from streamlit_option_menu import option_menu
 import sys
 st.set_page_config(page_title="AI-FitnessTrainer", page_icon=":basketball:", layout="wide")
-sys.path.append('./AI-Trainer')
+sys.path.append('/AI-Trainer')
 from exercise_analyzer import ExerciseAnalyzer
 from constants import _TEXT_BICEPS, _TEXT_PUSH_UP, _TEXT_REVERSE_PUSH_UP, _TEXT_SQUAT, _MAIN_PAGE_TEXT
 
