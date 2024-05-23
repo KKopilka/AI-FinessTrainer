9892202c5b7263e5c514ad392d4210c0294f1597
sikOo <siko.sik.1@gmail.com>
2024-05-23 21:30:38 +0300

    configs

---
+++ Dockerfile
@@ -0,0 +1,95 @@
+# app/Dockerfile
+
+FROM python:3.10-slim
+
+#WORKDIR /app
+
+# OpenCV Version
+ARG OPENCV_VERSION="4.8.0"
+
+# Install build dependencies
+RUN apt-get clean && \
+    apt-get update && \
+    apt-get install -y --no-install-recommends --fix-missing \
+        build-essential binutils \
+        ca-certificates cmake cmake-qt-gui curl \
+        dbus-x11 \
+        ffmpeg \
+        gdb gcc g++ gfortran git \
+        tar \
+        lsb-release \
+        procps \
+        manpages-dev \
+        unzip \
+        zip \
+        wget \
+        xauth \
+        swig
+
+RUN apt-get install -y --no-install-recommends --fix-missing \
+        python3-pip python3-dev python3-numpy python3-distutils \
+        python3-setuptools python3-pyqt5 python3-opencv
+
+RUN apt-get install -y --no-install-recommends --fix-missing \
+        libboost-python-dev libboost-thread-dev libatlas-base-dev libavcodec-dev \
+        libavformat-dev libavutil-dev libcanberra-gtk3-module libeigen3-dev \
+        libglew-dev libgl1-mesa-dev libgl1-mesa-glx libglib2.0-0 libgtk2.0-dev
+RUN apt-get install -y --no-install-recommends --fix-missing \  
+        libgtk-3-dev libjpeg-dev libjpeg62-turbo-dev liblapack-dev \
+        liblapacke-dev libopenblas-dev libopencv-dev libpng-dev libpostproc-dev \
+        libpq-dev libsm6 libswscale-dev libtbb-dev libtbbmalloc2 libtesseract-dev \
+        libtiff-dev libtiff5-dev libv4l-dev libx11-dev libxext6 libxine2-dev \
+        libxrender-dev libxvidcore-dev libx264-dev libgtkglext1 libgtkglext1-dev
+
+RUN apt-get install -y --no-install-recommends --fix-missing \
+        libvtk9-dev libdc1394-dev libgstreamer-plugins-base1.0-dev  \
+        libgstreamer1.0-dev libopenexr-dev \
+        openexr \
+        pkg-config \
+        qv4l2 \
+        v4l-utils \
+        zlib1g-dev \
+        locales \
+        curl \
+        software-properties-common \
+        git \
+        && locale-gen en_US.UTF-8 \
+        && LC_ALL=en_US.UTF-8 \
+        && rm -rf /var/lib/apt/lists/* \
+        && apt-get clean
+
+WORKDIR /opencv
+RUN wget -O opencv.zip https://github.com/opencv/opencv/archive/${OPENCV_VERSION}.zip \
+    && wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/${OPENCV_VERSION}.zip \
+    && unzip opencv.zip \
+    && unzip opencv_contrib.zip \
+    && mv opencv-${OPENCV_VERSION} opencv \
+    && mv opencv_contrib-${OPENCV_VERSION} opencv_contrib
+
+RUN mkdir /opencv/opencv/build
+WORKDIR /opencv/opencv/build
+
+RUN cmake -D CMAKE_BUILD_TYPE=RELEASE \
+ -D CMAKE_INSTALL_PREFIX=/usr/local \
+ -D INSTALL_PYTHON_EXAMPLES=ON \
+ -D INSTALL_C_EXAMPLES=ON \
+ -D OPENCV_ENABLE_NONFREE=ON \
+ -D OPENCV_GENERATE_PKGCONFIG=ON \
+ -D OPENCV_EXTRA_MODULES_PATH=/opencv/opencv_contrib/modules \
+ -D PYTHON_EXECUTABLE=/usr/local/bin/python \
+ -D BUILD_EXAMPLES=ON .. \
+    && make -j$(nproc) && make install && ldconfig
+
+#RUN git clone https://github.com/streamlit/streamlit-example.git .
+
+WORKDIR /streamlit
+
+COPY . .
+
+RUN pip3 install -r requirements.txt
+
+EXPOSE 8501
+
+HEALTHCHECK CMD curl --fail http://localhost:8501/_stcore/health
+
+ENTRYPOINT ["streamlit", "run", "/streamlit/app/live.py", "--server.port=8501", "--server.address=0.0.0.0"]
--- app/live.py
+++ app/live.py
@@ -1,129 +1,43 @@
-# # import streamlit as st
-# # import av
-# # from streamlit_webrtc import VideoHTMLAttributes, webrtc_streamer
-# # from streamlit_option_menu import option_menu
-# # from aiortc.contrib.media import MediaRecorder
-# # import sys
-# # sys.path.append('\AI-Trainer')
-# # from exercise_analyzer import ExerciseAnalyzer
-# # from constants import _TEXT_BICEPS, _TEXT_PUSH_UP, _TEXT_REVERSE_PUSH_UP, _TEXT_SQUAT
-# # from ai_trainer.properties import *
-
-# # st.set_page_config(page_title="AI-FitnessTrainer", page_icon=":basketball:",layout="wide")
-# # st.header('Интеллектуальный помощник по фитнесу :rocket:', divider='gray')
-
-# # def stream_data_squat():
-# #     for word in _TEXT_SQUAT.split(" "):
-# #         yield word + " "
-# #         time.sleep(0.02)
-
-# # def stream_data_push_up():
-# #     for word in _TEXT_PUSH_UP.split(" "):
-# #         yield word + " "
-# #         time.sleep(0.02)
-    
-# # def stream_data_biceps():
-# #     for word in _TEXT_BICEPS.split(" "):
-# #         yield word + " "
-# #         time.sleep(0.02)
-
-# # def stream_data_reverse_push_up():
-# #     for word in _TEXT_REVERSE_PUSH_UP.split(" "):
-# #         yield word + " "
-# #         time.sleep(0.02)
-
-# # with st.sidebar:
-# #     # mode = st.radio('Выберите упражнение', [':basketball: Фронтальные приседания', 
-# #     #                                     ':basketball: Отжимания с широкой постановкой рук',
-# #     #                                     ':basketball: Упражнение с гантелями на бицепс',
-# #     #                                     ':basketball: Обратные отжимания'], index=None)
-# #     mode = option_menu('Выберите упражнение', 
-# #                                     ['Фронтальные приседания', 
-# #                                     'Отжимания с широкой постановкой рук',
-# #                                     'Упражнение с гантелями на бицепс',
-# #                                     'Обратные отжимания'], 
-# #                                     menu_icon='rocket-takeoff',
-# #                                     icons=['arrow-right-short', 'arrow-right-short', 'arrow-right-short', 'arrow-right-short'],
-# #                                     # styles={
-# #                                     #     "container": {"padding": "0!important", "background-color": "#fafafa"}
-# #                                     #     "icon": {"color": "orange", "font-size": "25px"}, 
-# #                                     #     "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
-# #                                     #     "nav-link-selected": {"background-color": "green"},
-# #                                     # },
-# #                                     default_index=1)
-
-# # left_column, right_column = st.columns(2)
-
-# # if mode == 'Фронтальные приседания':
-# #     with left_column:
-# #         st.image("./assets/frontalniye-prisedaniya.jpeg", width=400)
-# #     with right_column:
-# #         # st.write_stream(stream_data_squat)
-# #         st.write(_TEXT_SQUAT)
-        
-# #     analyzer = ExerciseAnalyzer('front_squat')
-
-# # elif mode == 'Отжимания с широкой постановкой рук':
-# #     with left_column:
-# #         st.image("./assets/push_up.jpg", width=400)
-# #     with right_column:
-# #         # st.write_stream(stream_data_push_up)
-# #         st.write(_TEXT_PUSH_UP)
-
-# #     analyzer = ExerciseAnalyzer('push_up')
-
-# # elif mode == 'Упражнение с гантелями на бицепс':
-# #     with left_column:
-# #         st.image("./assets/biceps.jpg", width=400)
-# #     with right_column:
-# #         # st.write_stream(stream_data_biceps)
-# #         st.write(_TEXT_BICEPS)
-
-# #     analyzer = ExerciseAnalyzer('biceps')
-
-# # elif mode == 'Обратные отжимания':
-# #     with left_column:
-# #         st.image("./assets/reverse_push_up.jpg", width=450)
-# #     with right_column:
-# #         # st.write_stream(stream_data_reverse_push_up)
-# #         st.write(_TEXT_REVERSE_PUSH_UP)
-
-# #     analyzer = ExerciseAnalyzer('reverse_push_up')
-
-
-# # def video_frame_callback(frame: av.VideoFrame):
-# #     frame = frame.to_ndarray(format="rgb24") 
-# #     try:
-# #         frame, correctCount, incorrectCount = analyzer.analyze_video(frame) 
-# #     except BaseException as b:
-# #         print(b)
-# #     return av.VideoFrame.from_ndarray(frame, format="rgb24")
-
-# # # def out_recorder_factory() -> MediaRecorder:
-# # #         return MediaRecorder(output_video_file)
-
-
-# # ctx = webrtc_streamer(
-# #     key="example",
-# #     video_frame_callback=video_frame_callback,
-# #     # rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},  # Add this config
-# #     media_stream_constraints={"video": {"width": {'min':480, 'ideal':480}}, "audio": False},
-# #     video_html_attrs=VideoHTMLAttributes(autoPlay=True, controls=False, muted=False, width=1000)    
-# #     # out_recorder_factory=out_recorder_factory
-# # )
-
-
-
 import streamlit as st
 import time
 import av
-from streamlit_webrtc import VideoHTMLAttributes, webrtc_streamer
-from streamlit_option_menu import option_menu
+from streamlit_webrtc import VideoHTMLAttributes, WebRtcMode, webrtc_streamer
+# from streamlit_option_menu import option_menu
 import sys
+import cv2
 st.set_page_config(page_title="AI-FitnessTrainer", page_icon=":basketball:", layout="wide")
-sys.path.append('/AI-Trainer')
+import numpy as np
+
+sys.path.append('/streamlit')
 from exercise_analyzer import ExerciseAnalyzer
 from constants import _TEXT_BICEPS, _TEXT_PUSH_UP, _TEXT_REVERSE_PUSH_UP, _TEXT_SQUAT
+
+def video_frame_callback(frame: av.VideoFrame):
+    fr = frame.to_ndarray(format="rgb24") 
+    try:
+        #frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    
+        frame, correctCount, incorrectCount = analyzer.analyze_video(fr) 
+        fr = frame
+        
+        # print()
+    except Exception as err:
+        print(f"Error while proccessing frame. {err}")
+        frame = cv2.cvtColor(fr, cv2.COLOR_RGB2GRAY)
+        frame = cv2.Canny(frame,100,200)
+        fr = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
+
+    try:
+        out = av.VideoFrame.from_ndarray(fr, format="rgb24")
+    except Exception as err:
+        print(f"Error on output")
+        return av.VideoFrame.from_ndarray(np.zeros((420,420,3), np.uint8), format="rgb24")
+    #     #frame = cv2.Canny(frame,100,200)
+    #     #frame, correctCount, incorrectCount = analyzer.analyze_video(frame) 
+    # except Exception as b:
+    #     print("Error while proccessing frame", b)
+
+    return out
 
 if 'init' not in st.session_state:
     st.session_state['init'] = True
@@ -190,168 +104,25 @@ else:
             analyzer = ExerciseAnalyzer('reverse_push_up')
 
 
-    def video_frame_callback(frame: av.VideoFrame):
-        frame = frame.to_ndarray(format="rgb24") 
-        # try:
-        frame, correctCount, incorrectCount = analyzer.analyze_video(frame) 
-        # except BaseException as b:
-            # print(b)
-        return av.VideoFrame.from_ndarray(frame, format="rgb24")
-
-    ctx = webrtc_streamer(
-        key="example",
-        video_frame_callback=video_frame_callback,
-        # rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},  # Add this config
-        media_stream_constraints={"video": {"width": {'min':480, 'ideal':480}}, "audio": False},
-        video_html_attrs=VideoHTMLAttributes(autoPlay=True, controls=False, muted=False, width=1000)    
-        # out_recorder_factory=out_recorder_factory
-    )
-    # def out_recorder_factory() -> MediaRecorder:
-    #         return MediaRecorder(output_video_file)
-
-
     if st.button('Вернуться на главную страницу'):
         st.session_state['exercise'] = None
 
+    try:
+        ctx = webrtc_streamer(
+            key="example",
+            video_frame_callback=video_frame_callback,
+            # rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},  # Add this config
+            media_stream_constraints={"video": {"width": {'min':480, 'ideal':480}}, "audio": False},
+            video_html_attrs=VideoHTMLAttributes(autoPlay=True, controls=False, muted=False, width=1000),
+            async_processing=True,
+            mode=WebRtcMode.SENDRECV,
+            rtc_configuration={
+                "iceServers":[{"urls":"stun:stun.relay.metered.ca:80"},{"urls":"turn:global.relay.metered.ca:80","username":"fcb6fee592afde5c56516e8e","credential":"fS7LFEKdFz8vsRJz"},{"urls":"turn:global.relay.metered.ca:80?transport=tcp","username":"fcb6fee592afde5c56516e8e","credential":"fS7LFEKdFz8vsRJz"},{"urls":"turn:global.relay.metered.ca:443","username":"fcb6fee592afde5c56516e8e","credential":"fS7LFEKdFz8vsRJz"},{"urls":"turns:global.relay.metered.ca:443?transport=tcp","username":"fcb6fee592afde5c56516e8e","credential":"fS7LFEKdFz8vsRJz"}]
+            },
+            # out_recorder_factory=out_recorder_factory
+        )
+    except Exception as err:
+        print("Error on webrtc_streamer init. Error: ", err)
 
-
-# import streamlit as st
-# import av
-# from streamlit_webrtc import VideoHTMLAttributes, webrtc_streamer
-# from aiortc.contrib.media import MediaRecorder
-# import sys
-# sys.path.append('\AI-Trainer')
-# from exercise_analyzer import ExerciseAnalyzer
-
-# from ai_trainer.properties import *
-
-
-# st.set_page_config(layout="wide")
-# st.header('Интеллектуальный помощник по фитнесу :rocket:', divider='gray')
-
-# mode = st.radio('Выберите упражнение', [':basketball: Фронтальные приседания', 
-#                                         ':basketball: Отжимания с широкой постановкой рук',
-#                                         ':basketball: Упражнение с гантелями на бицепс',
-#                                         ':basketball: Обратные отжимания'], index=None)
-
-# _TEXT_SQUAT = """
-#         *Фронтальные приседания* — это упражнение с гантелями или штангой, при котором вы опускаетесь в приседание, удерживая груз перед собой на уровне груди.
-#         Оно направлено на развитие силы ног, особенно квадрицепсов, ягодиц и спины. Упражнение также требует хорошей координации и равновесия, что делает его эффективным для функциональной тренировки всего тела.
-
-#         ❗ Для правильного выполнения упражнения следует соблюдать следующие правила:\n
-#             1. Ваши ноги должны быть на ширине плеч.\n
-#             2. Локти не дожны быть сильно опущены вниз.\n
-#             3. Направление коленей во время приседаний должно быть строго по направлению
-#                носков.
-#     """
-
-# _TEXT_PUSH_UP = """
-#         *Отжимания с широкой постановкой рук* — это вариант отжиманий, которые отлично подходят для развития силы грудных мышц и укрепления верхней части тела.
-#         Это упражнение также способствует улучшению стабильности и контроля над телом. При правильном выполнении оно может быть эффективным инструментом для укрепления мышц и повышения общей физической формы.
-
-#         ❗ Для правильного выполнения упражнения следует соблюдать следующие правила:\n
-#             1. Ваши руки должны располагаться на ширине больше, чем ширина плеч.\n
-#             2. Локти не должны быть направлены сильно в стороны.
-#                Старайтесь слегка отвести их назад.\n
-#             3. Держите спину прямо.
-#     """
-
-# _TEXT_BICEPS = """
-#         *Упражнение с гантелями на бицепс* — это эффективное упражнение на бицепсы, которое можно выполнять как стоя, так и сидя. Основная цель этого упражнения — укрепление бицепсов и улучшение формы рук.
-#         Упражнение выполняется с гантелями, что позволяет работать с каждой рукой отдельно, обеспечивая равномерное развитие мышц и помогая исправлять асимметрию.
-
-#         ❗ Для правильного выполнения упражнения следует соблюдать следующие правила:\n
-#             1. Ваши ноги должны быть на ширине плеч.\n
-#             2. При подъеме гантели не старайтесь поднять ее засчет маха руки.
-#                Старайте поднимать гантель именно бицепсом.\n
-#             3. Локти должны быть зафиксированы у тела.\n
-#             4. Держите спину прямо и не отводите таз вперед.
-#     """
-
-# _TEXT_REVERSE_PUSH_UP = """
-#         *Обратные отжимания* — это не только отличное упражнение для формирования выразительных плеч, но и замечательный способ укрепить верхнюю часть спины,
-#         сделать её более подтянутой и снизить риск возникновения болей в плечевых суставах.
-#         Упражнение выполняется с использованием стула, скамьи или параллельных брусьев, и оно подходит как для начинающих, так и для опытных спортсменов.
-
-#         ❗ Для правильного выполнения упражнения следует соблюдать следующие правила:\n
-#             1. При выполнении упражнения не разводите локти слишком сильно в стороны.\n
-#             2. Таз должен находиться близко к скамье или стулу.\n
-#             3. Руки находнятся примерно на ширине плеч или чуть шире.\n
-#             4. Держите спину прямо.
-#     """
-
-# def stream_data_squat():
-#     for word in _TEXT_SQUAT.split(" "):
-#         yield word + " "
-#         time.sleep(0.02)
-
-# def stream_data_push_up():
-#     for word in _TEXT_PUSH_UP.split(" "):
-#         yield word + " "
-#         time.sleep(0.02)
-    
-# def stream_data_biceps():
-#     for word in _TEXT_BICEPS.split(" "):
-#         yield word + " "
-#         time.sleep(0.02)
-
-# def stream_data_reverse_push_up():
-#     for word in _TEXT_REVERSE_PUSH_UP.split(" "):
-#         yield word + " "
-#         time.sleep(0.02)
-
-# left_column, right_column = st.columns(2)
-
-# if mode == ':basketball: Фронтальные приседания':
-#     with left_column:
-#         st.image("./assets/frontalniye-prisedaniya.jpeg", width=650)
-#     with right_column:
-#         st.write_stream(stream_data_squat)
-        
-#     analyzer = ExerciseAnalyzer('front_squat')
-
-# elif mode == ':basketball: Отжимания с широкой постановкой рук':
-#     with left_column:
-#         st.image("./assets/push_up.jpg", width=650)
-#     with right_column:
-#         st.write_stream(stream_data_push_up)
-
-#     analyzer = ExerciseAnalyzer('push_up')
-
-# elif mode == ':basketball: Упражнение с гантелями на бицепс':
-#     with left_column:
-#         st.image("./assets/biceps.jpg", width=650)
-#     with right_column:
-#         st.write_stream(stream_data_biceps)
-
-#     analyzer = ExerciseAnalyzer('biceps')
-
-# elif mode == ':basketball: Обратные отжимания':
-#     with left_column:
-#         st.image("./assets/reverse_push_up.jpg", width=650)
-#     with right_column:
-#         st.write_stream(stream_data_reverse_push_up)
-
-#     analyzer = ExerciseAnalyzer('reverse_push_up')
-
-
-# def video_frame_callback(frame: av.VideoFrame):
-#     frame = frame.to_ndarray(format="rgb24") 
-#     try:
-#         frame, correctCount, incorrectCount = analyzer.analyze_video(frame) 
-#     except BaseException as b:
-#         print(b)
-#     return av.VideoFrame.from_ndarray(frame, format="rgb24")
-
-# # def out_recorder_factory() -> MediaRecorder:
-# #         return MediaRecorder(output_video_file)
-
-
-# ctx = webrtc_streamer(
-#     key="example",
-#     video_frame_callback=video_frame_callback,
-#     # rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},  # Add this config
-#     media_stream_constraints={"video": {"width": {'min':480, 'ideal':480}}, "audio": False},
-#     video_html_attrs=VideoHTMLAttributes(autoPlay=True, controls=False, muted=False, width=1000)    
-#     # out_recorder_factory=out_recorder_factory
-# )
+    # def out_recorder_factory() -> MediaRecorder:
+    #         return MediaRecorder(output_video_file)
